53aa46c8440b75ca048c6a7ff9544340d6ab96c0
KylHA <programlamakitabi@gmail.com>
2020-03-18 00:59:11 +0300

    Commit
    Project Commit

---
+++ Assets/Scripts/FormationControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationControl : MonoBehaviour
+{
+    public Transform Target;
+    [SerializeField] Transform DronePosition;
+
+    public GameObject TargetOBJ;
+    List<GameObject> Targets = new List<GameObject>();
+
+    private void Start()
+    {
+        Spawn_Targets_to_Formation();
+    }
+
+    void Spawn_Targets_to_Formation()
+    {
+        int DroneCount = GameObject.FindGameObjectsWithTag("Player").Length;
+        int countobj = 0;
+        for (int i = 0; i < DroneCount; i++)
+        {
+            Targets.Add(Instantiate(TargetOBJ, TargetOBJ.transform.position, TargetOBJ.transform.rotation));
+        }
+
+        foreach (var _target in Targets)
+        {
+            _target.transform.position = GameObject.FindGameObjectsWithTag("Player")[countobj].transform.position;
+
+            if (countobj < DroneCount)
+                countobj++;
+        }
+    }
+
+    public Vector3 CalculateDirection(Vector3 _Target, Vector3 _Obj)
+    {
+        Vector3 direction = _Target - _Obj;
+
+        return direction.normalized;
+    }
+
+    void PreDetermined_V_Formation()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //int targetCount = Targets.Count;
+
+            //if (targetCount % 2 != 0)
+            //    for (int i = 1; i <= targetCount; i++)
+            //    {
+            //        Targets[targetCount].transform.position=Vector3.up;
+            //    }
+
+            foreach (var item in Targets)
+            {
+                item.transform.position = new Vector3(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+            }
+
+        }
+    }
+
+    Vector3 FindClosestTarget()
+    {
+        return Vector3.up;
+    }
+}
---
+++ Assets/Scripts/UpDownLevFwdBck.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownLevFwdBck : MonoBehaviour
+{
+
+    public float UpDownForce;
+    FormationControl Fcontrol;
+
+    void Start()
+    {
+        UpDownForce = 9.81f;
+        Fcontrol = GameObject.Find("GameController").GetComponent<FormationControl>();
+    }
+
+
+    void FixedUpdate()
+    {
+        // GoPoz(Fcontrol.CalculateDirection(), 2);
+    }
+
+    //void UpDwnLev()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.I))
+    //        UpDownForce = 400f;
+
+    //    else if (Input.GetKeyDown(KeyCode.K))
+    //        UpDownForce = -200f;
+
+    //    else if (Input.GetKeyDown(KeyCode.L))
+    //    {
+    //        GetComponent<Rigidbody>().Sleep();
+    //        GetComponent<Rigidbody>().WakeUp();
+    //        UpDownForce = 9.81f;
+    //    }
+
+    //    else
+    //        UpDownForce = 9.81f;
+    //}
+
+    void GoPoz(Vector3 _poz, float speed)
+    {
+        // GetComponent<Rigidbody>().MovePosition(transform.position + control.CalculateDirection() * Time.fixedDeltaTime * speed);
+    }
+
+
+}
